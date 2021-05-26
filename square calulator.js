43595d052f89084c82272cc13c74ba97c339ee5b
creative10-13 <76442558+creative10-13@users.noreply.github.com>
2021-05-26 10:11:18 -0400

    Add files via upload

---
+++ square calulator.js
@@ -0,0 +1,8 @@
+var inputVal1 = document.getElementById("input1").value;
+var inputVal2 = document.getElementById("input2").value;
+while (true) {
+    if (ButtonPressed = 1) {
+        var calculation = inputVal1 + inputVal2;
+        document.getElementById("Answer").innerHTML = calculation;
+    }else{ButtonPressed = 0}
+}
